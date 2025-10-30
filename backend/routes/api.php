299773989ef037cdd2f660b7dceb008cc884e9b1
NobreNeon <rafab22santos@gmail.com>
2025-10-30 16:19:12 -0300

    feat(auth): implements authentication with Laravel Sanctum and secures API routes.
    - Added AuthController with login and logout
    - Configured auth:sanctum middleware
    - Updated protected routes in the api.php file
    - Included token generation and revocation
    
    Refs: #SCRUM-1

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,13 +1,28 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 
-//Usuários
-Route::get('/users', [UserController::class, 'index']);
+//Auth
+Route::post('/login', [AuthController::class, 'login']);
+
+//Users (Register)
 Route::post('/user', [UserController::class, 'store']);
-Route::get('/user/{id}', [UserController::class, 'show']);
-Route::put('/user/{id}', [UserController::class, 'update']);
-Route::delete('/user/{id}', [UserController::class, 'destroy']);
+
+Route::middleware('auth:sanctum')->group(function () {
+
+    //Auth
+    Route::post('/logout', [AuthController::class, 'logout']);
+
+    //Users
+    Route::get('/users', [UserController::class, 'index']);
+    Route::get('/user/{id}', [UserController::class, 'show']);
+    Route::put('/user/{id}', [UserController::class, 'update']);
+    Route::delete('/user/{id}', [UserController::class, 'destroy']);
+
+});
+
+
 
